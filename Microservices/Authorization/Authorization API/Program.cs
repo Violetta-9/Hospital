22041dc3ffde5b60d887b9dd2ce39ce718fd92c9
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-20 18:27:13 +0300

    Programm update

---
+++ Microservices/Authorization/Authorization API/Program.cs
@@ -0,0 +1,28 @@
+using Authorization.Data.EF.PostgreSQL;
+
+var builder = WebApplication.CreateBuilder(args);
+var services=builder.Services;
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+services.AddHospitalPostgreSQL(builder.Configuration.GetSection("ConnectionStrings:DefaultConnection").Value);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
