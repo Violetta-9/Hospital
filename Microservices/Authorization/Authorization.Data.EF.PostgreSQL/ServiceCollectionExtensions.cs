3505ab44f6fb3101c0680a25b1e54f955ebf3b12
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-20 18:26:48 +0300

    Ef

---
+++ Microservices/Authorization/Authorization.Data.EF.PostgreSQL/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Shared.DbContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Authorization.Data.EF.PostgreSQL
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void AddHospitalPostgreSQL(this IServiceCollection services,string connectionString)
+        {
+            services.AddDbContext<HospitalDbContext>(opt => opt.UseLazyLoadingProxies().UseNpgsql(connectionString));
+        }
+    }
+}
