3f732a6274fd29c0fbf5e7027bc6964fd7819dad
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-20 18:25:46 +0300

    DbContext

---
+++ Microservices/Authorization/Authorization.Data.Shared/DbContext/HospitalDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Authorization.Data.Shared.Interseptor;
+using Authorization.Data_Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.Data.Shared.DbContext
+{
+    public class HospitalDbContext:IdentityDbContext<Account>
+    {
+        public DbSet<Photo> Photos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+     => optionsBuilder.AddInterceptors(new AuditableInterseptor());
+    }
+}
