2b7299ce1553127b58356e0e5e7cd04b88b0bcbc
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-20 18:23:41 +0300

    Add entity

---
+++ Microservices/Authorization/Authorization.Data,Domain/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using Authorization.Data_Domain.Models.Abstraction;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authorization.Data_Domain.Models
+{
+    public class Account:IdentityUser, IAudientable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTimeOffset RowCreatedTimestamp { get; set; }
+        public DateTimeOffset LastRowModificationTimestamp { get; set; }
+        public long PhotoId { get; set; }
+        public virtual Photo Photo { get; set; }
+    }
+}
---
+++ Microservices/Authorization/Authorization.Data,Domain/Models/Photo.cs
@@ -0,0 +1,11 @@
+﻿
+using Authorization.Data_Domain.Models.Abstraction;
+
+namespace Authorization.Data_Domain.Models
+{
+    public class Photo: KeyedEntityBase
+    {
+        public string Url { get; set; }
+        public virtual Account Account { get; set; }
+    }
+}
