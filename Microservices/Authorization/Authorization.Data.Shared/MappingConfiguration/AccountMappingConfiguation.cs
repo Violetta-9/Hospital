74fd300aa1dae5f93047e199fae16d14781430ee
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-20 18:26:27 +0300

    MappingConfigure

---
+++ Microservices/Authorization/Authorization.Data.Shared/MappingConfiguration/AccountMappingConfiguation.cs
@@ -0,0 +1,23 @@
+﻿using Authorization.Data.Shared.MappingConfiguration.Extension;
+using Authorization.Data_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Authorization.Data.Shared.MappingConfiguration
+{
+    public class AccountMappingConfiguation : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+           builder.MapAuditProperty();
+            builder.Property(x => x.FirstName).HasColumnName(nameof(Account.FirstName)).HasMaxLength(256)
+              .IsRequired();
+
+            builder.Property(x => x.LastName).HasColumnName(nameof(Account.LastName)).HasMaxLength(256).IsRequired();
+            builder.Property(x => x.MiddleName).HasColumnName(nameof(Account.MiddleName)).HasMaxLength(256).IsRequired();
+            builder.HasOne(x => x.Photo).WithOne(x => x.Account);
+            
+        }
+    }
+}
---
+++ Microservices/Authorization/Authorization.Data.Shared/MappingConfiguration/PhotoMappingConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Authorization.Data.Shared.MappingConfiguration.Extension;
+using Authorization.Data_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Authorization.Data.Shared.MappingConfiguration
+{
+    public class PhotoMappingConfiguration : IEntityTypeConfiguration<Photo>
+    {
+        public void Configure(EntityTypeBuilder<Photo> builder)
+        {
+            builder.BasedMapping();
+            builder.Property(x => x.Url).HasColumnName(nameof(Photo.Url)).HasMaxLength(516).IsRequired();
+        }
+    }
+}
