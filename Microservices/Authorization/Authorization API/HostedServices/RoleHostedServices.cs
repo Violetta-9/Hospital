6fdcc1209cffdc188ddc087b7ebea42248cf6c91
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-22 23:00:43 +0300

    HostedServices

---
+++ Microservices/Authorization/Authorization API/HostedServices/RoleHostedServices.cs
@@ -0,0 +1,48 @@
+﻿using Authorization.Application.Helpers;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authorization_API.HostedServices
+{
+    public class RoleHostedServices:IHostedService
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public RoleHostedServices(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            if (!await roleManager.RoleExistsAsync(UserRoles.Admin))
+            {
+                var role = new IdentityRole(UserRoles.Admin);
+                await roleManager.CreateAsync(role);
+            }
+
+            if (!await roleManager.RoleExistsAsync(UserRoles.User))
+            {
+                var role = new IdentityRole(UserRoles.User);
+                await roleManager.CreateAsync(role);
+            }
+            if (!await roleManager.RoleExistsAsync(UserRoles.Doctor))
+            {
+                var role = new IdentityRole(UserRoles.Doctor);
+                await roleManager.CreateAsync(role);
+            }
+            if (!await roleManager.RoleExistsAsync(UserRoles.Patient))
+            {
+                var role = new IdentityRole(UserRoles.Patient);
+                await roleManager.CreateAsync(role);
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
