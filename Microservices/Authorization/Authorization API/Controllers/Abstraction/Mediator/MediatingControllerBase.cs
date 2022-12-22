c2fdbfd8c4dae9ab7aadf3e195786df3cb9abe82
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-22 22:11:19 +0300

    Controller

---
+++ Microservices/Authorization/Authorization API/Controllers/Abstraction/Mediator/MediatingControllerBase.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IIS.Core;
+
+namespace Authorization_API.Controllers.Abstraction.Mediator
+{
+    public class MediatingControllerBase:ControllerBase
+    {
+        protected readonly IMediator _mediator;
+        public MediatingControllerBase(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task<ActionResult> SendRequestAsync<T>(T request,CancellationToken cancellation=default)
+        {
+
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            var result = await _mediator.Send(request);
+            if (result is null)
+            {
+                return NotFound(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
---
+++ Microservices/Authorization/Authorization API/Controllers/User/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Authorization.Application.Command.User.Registration;
+using Authorization.Application.Contracts.Incoming.User;
+using Authorization.Application.Contracts.Outgoing;
+using Authorization.Application.Query.User;
+using Authorization_API.Controllers.Abstraction.Mediator;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Authorization_API.Controllers.User
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : MediatingControllerBase
+    {
+        public UserController(IMediator mediator) : base(mediator)
+        {
+        }
+        [HttpPost("registration")]
+        [SwaggerOperation(Summary = "Register new user", OperationId = "RegisterUser")]
+        [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(string))]
+        public async Task<ActionResult> Registration([FromBody]UserDTO newUser)
+        {
+            var query = new RegistrationCommand(newUser);
+            return await SendRequestAsync(query);
+        }
+        [HttpPost("login")]
+     [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(string))]
+        [SwaggerOperation(Summary = "Login", OperationId = "Login")]
+        public async Task<ActionResult> Login([FromForm] LoginDTO login)
+        {
+            var query = new LoginQuery(login);
+            return await SendRequestAsync(query);
+        }
+    }
+}
