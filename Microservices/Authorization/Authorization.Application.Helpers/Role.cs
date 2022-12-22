a3dedf1c694c6142113baabbebff2e383c5bca3f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-22 23:01:09 +0300

    Role

---
+++ Microservices/Authorization/Authorization.Application.Helpers/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization.Application.Helpers
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string Doctor = "Doctor";
+        public const string Patient = "Patient";
+    }
+}
