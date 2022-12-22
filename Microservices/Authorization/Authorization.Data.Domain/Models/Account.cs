ff7d94674871ae174b422128d273e8156cd9944a
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-22 22:12:05 +0300

    Update model

---
+++ Microservices/Authorization/Authorization.Data.Domain/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using Authorization.Data_Domain.Models.Abstraction;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authorization.Data_Domain.Models
+{
+    public class Account:IdentityUser, IAudientable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTimeOffset RowCreatedTimestamp { get; set; }
+        public DateTimeOffset LastRowModificationTimestamp { get; set; }
+    }
+}
