70ef280df3f85a5a5db303392cd8903f75cb9b0d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-29 11:42:01 +0300

    update application

--- Microservices/Profile/Profile.Application/Command/Receptionists/AddReceptionistRole/AddReceptionistCommandHandler.cs
+++ Microservices/Profile/Profile.Application/Command/Receptionists/AddReceptionistRole/AddReceptionistCommandHandler.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Profile.Application.Contracts.Outgoing;
 using Profile.Application.Helpers;
 
-namespace Profile.Application.Command.Admin
+namespace Profile.Application.Command.Receptionists.AddReceptionistRole
 {
     public class AddReceptionistCommandHandler : IRequestHandler<AddReceptionistRoleCommand, Response>
     {
@@ -23,16 +23,16 @@ namespace Profile.Application.Command.Admin
         {
             var role = UserRoles.Receptionist;
             var user = await _userManager.FindByIdAsync(request.UserId);
-            if (user!= null )
+            if (user != null)
             {
-               await _userManager.AddToRoleAsync(user,role);
-               await _receptionistRepository.InsertAsync(new Receptionist()
-               {
-                   AccountId = user.Id,
-                   OfficeId = request.OfficeId,
+                await _userManager.AddToRoleAsync(user, role);
+                await _receptionistRepository.InsertAsync(new Receptionist()
+                {
+                    AccountId = user.Id,
+                    OfficeId = request.OfficeId,
 
-               }, cancellationToken);
-               return Response.Success;
+                }, cancellationToken);
+                return Response.Success;
             }
             return Response.Error;
         }
