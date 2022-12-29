ac78e74b89e3d25efe9390a5f2884ebee3ea1e08
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-29 14:54:48 +0300

    Query for getting  Doctor by Id

---
+++ Microservices/Profile/Profile.Application/Query/Doctor/GetDoctorById/GetDoctorByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Profile.Application.Contracts.Outgoing;
+
+namespace Profile.Application.Query.Doctor.GetDoctorById
+{
+    public class GetDoctorByIdQueryHandler : IRequestHandler<GetDoctorByIdQuery, DoctorOneDTO>
+    {
+        private readonly IDoctorRepository _doctorRepository;
+
+        public GetDoctorByIdQueryHandler(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+        public async Task<DoctorOneDTO> Handle(GetDoctorByIdQuery request, CancellationToken cancellationToken)
+        {
+          return await _doctorRepository.GetDoctorById(request.DoctorId,cancellationToken);
+        }
+    }
+}
