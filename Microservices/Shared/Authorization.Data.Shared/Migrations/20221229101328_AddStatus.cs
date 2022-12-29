8b4fd95b475a1550b35597c0f990660596824262
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-29 14:47:27 +0300

    Seed Statuses

---
+++ Microservices/Shared/Authorization.Data.Shared/Migrations/20221229101328_AddStatus.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Authorization.Data.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Doctors");
+
+            migrationBuilder.AddColumn<long>(
+                name: "StatusId",
+                table: "Doctors",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "Statuses",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    RowCreatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    LastRowModificationTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Statuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_StatusId",
+                table: "Doctors",
+                column: "StatusId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Doctors_Statuses_StatusId",
+                table: "Doctors",
+                column: "StatusId",
+                principalTable: "Statuses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Doctors_Statuses_StatusId",
+                table: "Doctors");
+
+            migrationBuilder.DropTable(
+                name: "Statuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Doctors_StatusId",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "StatusId",
+                table: "Doctors");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Status",
+                table: "Doctors",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
