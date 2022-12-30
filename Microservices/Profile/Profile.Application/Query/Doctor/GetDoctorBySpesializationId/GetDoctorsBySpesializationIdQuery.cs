badfb323ed2bafa897b48304dea317e729a67e1f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-30 16:38:33 +0300

    Get doctor by spezialization Id

---
+++ Microservices/Profile/Profile.Application/Query/Doctor/GetDoctorBySpesializationId/GetDoctorsBySpesializationIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Profile.Application.Contracts.Outgoing;
+
+namespace Profile.Application.Query.Doctor.GetDoctorBySpesializationId
+{
+    public class GetDoctorsBySpesializationIdQuery : IRequest<DoctorAllDTO[]>
+    {
+        public long SpesializationId { get; set; }
+        public GetDoctorsBySpesializationIdQuery(long spesializationId)
+        {
+            SpesializationId = spesializationId;
+        }
+    }
+}
