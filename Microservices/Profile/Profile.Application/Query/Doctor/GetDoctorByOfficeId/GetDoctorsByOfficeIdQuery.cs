2ecbff606b69ecc4a4f95c26e4dc67271796a3f7
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-30 16:38:12 +0300

    Get Doctor By office Id

---
+++ Microservices/Profile/Profile.Application/Query/Doctor/GetDoctorByOfficeId/GetDoctorsByOfficeIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Profile.Application.Contracts.Outgoing;
+
+namespace Profile.Application.Query.Doctor.GetDoctorByOfficeId
+{
+    public class GetDoctorsByOfficeIdQuery : IRequest<DoctorAllDTO[]>
+    {
+        public long OfficeId { get; set; }
+
+        public GetDoctorsByOfficeIdQuery(long officeId)
+        {
+            OfficeId = officeId;
+        }
+    }
+}
