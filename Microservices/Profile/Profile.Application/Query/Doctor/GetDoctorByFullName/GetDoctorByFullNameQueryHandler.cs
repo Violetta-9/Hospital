8edc974ec1dad2f251003bb2f012e2e0bc3986a2
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2022-12-30 16:37:49 +0300

    Get doctor by fullName

---
+++ Microservices/Profile/Profile.Application/Query/Doctor/GetDoctorByFullName/GetDoctorByFullNameQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Profile.Application.Contracts.Outgoing;
+
+namespace Profile.Application.Query.Doctor.GetDoctorByFullName
+{
+    public class GetDoctorByFullNameQueryHandler : IRequestHandler<GetDoctorByFullNameQuery, DoctorAllDTO[]>
+    {
+        private readonly IDoctorRepository _doctorRepository;
+        public GetDoctorByFullNameQueryHandler(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        public async Task<DoctorAllDTO[]> Handle(GetDoctorByFullNameQuery request, CancellationToken cancellationToken)
+        {
+            return await _doctorRepository.FindDoctorByFullNameAsync(request.DoctorsFullName.FirstName,
+                request.DoctorsFullName.LastName, request.DoctorsFullName.MiddleName, cancellationToken);
+        }
+    }
+}
