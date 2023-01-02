05e737138d9915d183a5c2dcebb8c714df6b872d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-02 14:49:04 +0300

    Get all receptionists

---
+++ Microservices/Profile/Profile.Application/Query/Receptionist/GetAllReceptionist/GetAllReceptionistQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Profile.Application.Contracts.Outgoing;
+
+namespace Profile.Application.Query.Receptionist.GetAllReceptionist
+{
+    public class GetAllReceptionistQuery : IRequest<ReceptionistAllDTO[]>
+    {
+    }
+}
---
+++ Microservices/Profile/Profile.Application/Query/Receptionist/GetAllReceptionist/GetAllReceptionistQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Profile.Application.Contracts.Outgoing;
+
+namespace Profile.Application.Query.Receptionist.GetAllReceptionist
+{
+    internal class GetAllReceptionistQueryHandler : IRequestHandler<GetAllReceptionistQuery, ReceptionistAllDTO[]>
+    {
+        private readonly IReceptionistRepository _receptionistRepository;
+
+        public GetAllReceptionistQueryHandler(IReceptionistRepository receptionistRepository)
+        {
+            _receptionistRepository=receptionistRepository;
+        }
+
+        public async Task<ReceptionistAllDTO[]> Handle(GetAllReceptionistQuery request, CancellationToken cancellationToken)
+        {
+            return await _receptionistRepository.GetAllReceptionistAsync(cancellationToken);
+        }
+    }
+}
