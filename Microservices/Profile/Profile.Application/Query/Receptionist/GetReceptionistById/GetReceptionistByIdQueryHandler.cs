30b4376e76f3fb056091372767d95b5efb32d9c8
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-02 14:48:45 +0300

    Get receptionist by id

---
+++ Microservices/Profile/Profile.Application/Query/Receptionist/GetReceptionistById/GetReceptionistByIdQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Profile.Application.Contracts.Outgoing;
+
+namespace Profile.Application.Query.Receptionist.GetReceptionistById
+{
+    internal class GetReceptionistByIdQueryHandler : IRequestHandler<GetReceptionistByIdQuery, ReceptionistOneDTO>
+    {
+        private readonly IReceptionistRepository _receptionistRepository;
+
+        public GetReceptionistByIdQueryHandler(IReceptionistRepository receptionistRepository)
+        {
+            _receptionistRepository = receptionistRepository;
+        }
+        public async  Task<ReceptionistOneDTO> Handle(GetReceptionistByIdQuery request, CancellationToken cancellationToken)
+        {
+           return await _receptionistRepository.GetReceptionistByIdAsync(request.ReceptionistId, cancellationToken);
+        }
+    }
+}
