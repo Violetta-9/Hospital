955ff5f86970d4d7f7f21f9fa2bd221bfcdd45af
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-02 20:23:15 +0300

    Migration

---
+++ Microservices/Shared/Authorization.Data.Shared/Migrations/20230102165929_CreateMappingConfiguration.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Authorization.Data.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMappingConfiguration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Receptionists_AccountId",
+                table: "Receptionists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_AccountId",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Doctors_AccountId",
+                table: "Doctors");
+
+            migrationBuilder.RenameColumn(
+                name: "Birtday",
+                table: "AspNetUsers",
+                newName: "Birthday");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Receptionists_AccountId",
+                table: "Receptionists",
+                column: "AccountId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_AccountId",
+                table: "Patients",
+                column: "AccountId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_AccountId",
+                table: "Doctors",
+                column: "AccountId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Receptionists_AccountId",
+                table: "Receptionists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Patients_AccountId",
+                table: "Patients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Doctors_AccountId",
+                table: "Doctors");
+
+            migrationBuilder.RenameColumn(
+                name: "Birthday",
+                table: "AspNetUsers",
+                newName: "Birtday");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Receptionists_AccountId",
+                table: "Receptionists",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patients_AccountId",
+                table: "Patients",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doctors_AccountId",
+                table: "Doctors",
+                column: "AccountId");
+        }
+    }
+}
