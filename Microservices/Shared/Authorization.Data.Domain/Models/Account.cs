ed4ed54a44fc7b6745db6909a3e2f443de9b18d8
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-02 20:21:11 +0300

    Update Account entity

--- Microservices/Shared/Authorization.Data.Domain/Models/Account.cs
+++ Microservices/Shared/Authorization.Data.Domain/Models/Account.cs
@@ -8,11 +8,11 @@ namespace Authorization.Data_Domain.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string MiddleName { get; set; }
-        public DateTime Birtday { get; set; }
+        public DateTime Birthday { get; set; }
         public DateTimeOffset RowCreatedTimestamp { get; set; }
         public DateTimeOffset LastRowModificationTimestamp { get; set; }
-        public virtual ICollection<Doctor> Doctors { get; set; }
-        public virtual ICollection<Receptionist> Receptionists { get; set; }
-        public virtual ICollection<Patient> Patients { get; set; }
+        public virtual Doctor Doctors { get; set; }
+        public virtual Receptionist Receptionists { get; set; }
+        public virtual Patient Patients { get; set; }
     }
 }
