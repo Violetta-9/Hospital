729925f60a46a8dd148bac35166fa4f965066720
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-02 11:00:16 +0300

    Get all patient

---
+++ Microservices/Profile/Profile.Application/Query/Patient/GetAllPatient/GetAllPatientQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Profile.Application.Contracts.Outgoing;
+
+namespace Profile.Application.Query.Patient.GetAllPatient
+{
+    internal class GetAllPatientQueryHandler : IRequestHandler<GetAllPatientQuery, PatientAllDTO[]>
+    {
+        private readonly IPatientRepository _patientRepository;
+
+        public GetAllPatientQueryHandler(IPatientRepository patientRepository)
+        {
+            _patientRepository=patientRepository;
+        }
+        public async Task<PatientAllDTO[]> Handle(GetAllPatientQuery request, CancellationToken cancellationToken)
+        {
+            return await _patientRepository.GetAllPatientsAsync(cancellationToken);
+        }
+    }
+}
