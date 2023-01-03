2c4b9f5ebf7675fe72c7657b5458add2d41aef1e
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-03 17:46:18 +0300

    Query validator for receptionist

---
+++ Microservices/Profile/Profile.Application/Validators/Query/Receptionist/GetReceptionistByIdValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using FluentValidation;
+using Profile.Application.Query.Receptionist.GetReceptionistById;
+using Profile.Application.Resources;
+
+namespace Profile.Application.Validators.Query.Receptionist
+{
+    internal class GetReceptionistByIdValidator:AbstractValidator<GetReceptionistByIdQuery>
+    {
+        private readonly IReceptionistRepository _receptionistRepository;
+        public GetReceptionistByIdValidator(IReceptionistRepository receptionistRepository)
+        {
+            _receptionistRepository = receptionistRepository;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.ReceptionistId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(opt => string.Format(Messages.EmptyField, nameof(opt.ReceptionistId)))
+                .MustAsync(ExistsReceptionist)
+                .WithMessage(opt => string.Format(Messages.NotFoundReceptionist, opt.ReceptionistId));
+        }
+
+        private async Task<bool> ExistsReceptionist(long receptionistId, CancellationToken cancellationToken)
+        {
+            var receptionist = await _receptionistRepository.GetReceptionistByIdAsync(receptionistId, cancellationToken);
+            return receptionist != null;
+        }
+    }
+}
