0d011a51b42809bd6a270e8c452edd81e2ff10a0
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-03 17:13:10 +0300

    Command validators for doctor

---
+++ Microservices/Profile/Profile.Application/Validators/Commands/Doctor/AssignDoctorRoleValidator.cs
@@ -0,0 +1,73 @@
+﻿
+using Authorization.Data_Domain.Models;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Profile.Application.Command.Doctors.AddDoctorRole;
+using Profile.Application.Resources;
+
+namespace Profile.Application.Validators.Commands.Doctor
+{
+    public class AssignDoctorRoleValidator: AbstractValidator<AddDoctorRoleCommand>
+    {
+        private readonly UserManager<Account> _userManager;
+
+        public AssignDoctorRoleValidator(UserManager<Account> userManager)
+        {
+            _userManager=userManager;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.Doctor.AccountId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(Messages.EmptyField)
+                .MustAsync(ExistsAccountAsync)
+                .WithMessage(opt => string.Format(Messages.NotFoundAccount, opt.Doctor.AccountId));
+            //todo: validation
+            //RuleFor(x=>x.Doctor.OfficeId)
+            //    .Cascade(CascadeMode.Stop)
+            //    .NotEmpty()
+            //    .WithMessage(Messages.EmptyField)
+            //    .MustAsync(ExistsOfficeAsync)
+            //    .WithMessage(opt => string.Format(Messages.NotFoundOffice, opt.Doctor.OfficeId));
+
+            //RuleFor(x => x.Doctor.StatusId)
+            //    .Cascade(CascadeMode.Stop)
+            //    .NotEmpty()
+            //    .WithMessage(Messages.EmptyField)
+            //    .MustAsync(ExistsStatusAsync)
+            //    .WithMessage(opt => string.Format(Messages.NotFoundStatus, opt.Doctor.StatusId));
+
+            //RuleFor(x => x.Doctor.SpecializationId)
+            //    .Cascade(CascadeMode.Stop)
+            //    .NotEmpty()
+            //    .WithMessage(Messages.EmptyField)
+            //    .MustAsync(ExistsSpecializationAsync)
+            //    .WithMessage(opt => string.Format(Messages.NotFoundSpecialition, opt.Doctor.SpecializationId));
+
+        }
+
+        //private Task<bool> ExistsSpecializationAsync(long specId, CancellationToken arg2)
+        //{
+            
+        //}
+
+        //private Task<bool> ExistsStatusAsync(long statusId, CancellationToken arg2)
+        //{
+          
+        //}
+
+        //private Task<bool> ExistsOfficeAsync(long officeId, CancellationToken arg2)
+        //{
+           
+        //}
+
+        private async Task<bool> ExistsAccountAsync(string id, CancellationToken cancellationToken)
+        {
+           var user=await _userManager.FindByIdAsync(id);
+           return user != null;
+        }
+    }
+}
---
+++ Microservices/Profile/Profile.Application/Validators/Commands/Doctor/UpdateDoctorStatusValidator.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Profile.Application.Command.Doctors.UpdateDoctor.UpdateDoctorProfile;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Profile.Application.Command.Doctors.UpdateDoctor.UpdateDoctorStatus;
+using Authorization.Data_Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Profile.Application.Resources;
+
+namespace Profile.Application.Validators.Commands.Doctor
+{
+    public class UpdateDoctorStatusValidator: AbstractValidator<UpdateDoctorStatusCommand>
+    {
+        private readonly UserManager<Account> _userManager;
+
+        public UpdateDoctorStatusValidator(UserManager<Account> userManager)
+        {
+            _userManager = userManager;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.AccountId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(Messages.EmptyField)
+                .MustAsync(ExistsAccountAsync)
+                .WithMessage(opt => string.Format(Messages.NotFoundAccount, opt.AccountId));
+            //todo: validation
+
+            //RuleFor(x => x.StatusId)
+            //    .Cascade(CascadeMode.Stop)
+            //    .NotEmpty()
+            //    .WithMessage(Messages.EmptyField)
+            //    .MustAsync(ExistsStatusAsync)
+            //    .WithMessage(opt => string.Format(Messages.NotFoundStatus, opt.StatusId));
+
+
+
+        }
+        //private Task<bool> ExistsStatusAsync(long statusId, CancellationToken arg2)
+        //{
+
+        //}
+
+
+        private async Task<bool> ExistsAccountAsync(string id, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            return user != null;
+        }
+    }
+}
