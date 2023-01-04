976ff1bf973d9ade845aa73d4891fe7dbc8f66d9
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-04 16:51:21 +0300

    Command validators for receptionist

---
+++ Microservices/Profile/Profile.Application/Validators/Commands/Receptionist/DeleteReceptionistValidator.cs
@@ -0,0 +1,35 @@
+﻿using Authorization.Data_Domain.Models;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Profile.Application.Resources;
+
+using Profile.Application.Command.Receptionists.DeleteReceptionist;
+
+namespace Profile.Application.Validators.Commands.Receptionist
+{
+    internal class DeleteReceptionistValidator : AbstractValidator<DeleteReceptionistCommand>
+    {
+        private readonly UserManager<Account> _userManager;
+
+        public DeleteReceptionistValidator(UserManager<Account> userManager)
+        {
+            _userManager = userManager;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.AccountId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(Messages.EmptyField)
+                .MustAsync(ExistsAccountAsync)
+                .WithMessage(opt => string.Format(Messages.NotFoundAccount, opt.AccountId));
+        }
+        private async Task<bool> ExistsAccountAsync(string id, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            return user != null;
+        }
+    }
+}
