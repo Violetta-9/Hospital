fea2780489b550d63462b650cd9f9b5729775c2f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-04 16:59:09 +0300

    EmailServices

---
+++ Microservices/Profile/Profile.Application.Services/EmailServices.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Profile.Application.Contracts.Internal;
+
+namespace Profile.Application.Services
+{
+    public interface IEmailServices
+    {
+        public Task SendEmailAsync(string recipientEmail, string subject, string content,CancellationToken cancellationToken);
+    }
+
+    public class EmailServices: IEmailServices
+    {
+        private readonly EmailSettings _emailSettings;
+        public EmailServices(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+        public async Task SendEmailAsync(string recipientEmail,string subject,string content,CancellationToken cancellationToken)
+        {
+            var smtpClient = new SmtpClient(_emailSettings.SmtpServer,_emailSettings.Port)
+            {
+                Credentials = new NetworkCredential(_emailSettings.Email, _emailSettings.Password),
+                EnableSsl = true
+
+        };
+
+            var from = new MailAddress(_emailSettings.Email,"Hospital");
+            var to = new MailAddress(recipientEmail);
+            using var mailMessage = new MailMessage(from, to)
+            {
+                
+                Subject = subject,
+                Body = content
+               
+            };
+
+            await smtpClient.SendMailAsync(mailMessage,cancellationToken);
+
+        }
+    }
+}
