4fd41a02e96039df34cdc74c19aae229f804de8f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-04 16:58:54 +0300

    Update dto for doctor receptionist

--- Microservices/Profile/Profile.Application.Contracts/Incoming/DoctorDTO.cs
+++ Microservices/Profile/Profile.Application.Contracts/Incoming/DoctorDTO.cs
@@ -14,7 +14,6 @@ namespace Profile.Application.Contracts.Incoming
         public string LastName { get; set; }
         public string MiddleName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public int Day { get; set; }
         public int Month { get; set; }
