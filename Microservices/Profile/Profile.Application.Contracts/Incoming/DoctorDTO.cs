acf9ac4242215df85adff88581ec0579a849535a
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-04 12:38:29 +0300

    Update ReceptionistDto and DoctorDto

--- Microservices/Profile/Profile.Application.Contracts/Incoming/DoctorDTO.cs
+++ Microservices/Profile/Profile.Application.Contracts/Incoming/DoctorDTO.cs
@@ -9,7 +9,16 @@ namespace Profile.Application.Contracts.Incoming
 {
     public class DoctorDTO
     {
-        public string AccountId { get; set; }
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
         public long SpecializationId { get; set; }
         public long OfficeId { get; set; }
         public long StatusId { get; set; }
--- Microservices/Profile/Profile.Application.Contracts/Incoming/ReceptionistDTO.cs
+++ Microservices/Profile/Profile.Application.Contracts/Incoming/ReceptionistDTO.cs
@@ -8,7 +8,15 @@ namespace Profile.Application.Contracts.Incoming
 {
     public class ReceptionistDTO
     {
-        public string AccountId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
         public long OfficeId { get; set; }
     }
 }
