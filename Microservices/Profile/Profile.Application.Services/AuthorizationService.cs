21c7816db936119ca5a214c1d18731b20204a1be
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-04 12:37:17 +0300

    Add AuthorizationServices

---
+++ Microservices/Profile/Profile.Application.Services/AuthorizationService.cs
@@ -0,0 +1,97 @@
+﻿
+
+using Newtonsoft.Json;
+using Profile.Application.Contracts.Incoming;
+using System.Text;
+
+
+namespace Profile.Application.Services
+{
+    public interface IAuthorizationService
+    {
+        public Task<string> SendDoctorInfoForRegistrationAsync(DoctorDTO request,
+            CancellationToken cancellationToken);
+
+        public Task<string> SendReceptionistInfoForRegistrationAsync(ReceptionistDTO request,
+            CancellationToken cancellationToken);
+
+    }
+
+    public  class AuthorizationService:IAuthorizationService
+    {
+        public async Task<string> SendDoctorInfoForRegistrationAsync(DoctorDTO request,CancellationToken cancellationToken)
+        {
+            var password = Guid.NewGuid().ToString().Substring(0, 8);
+            var response = string.Empty;
+            Uri u = new Uri("https://localhost:44336/api/User/registration");
+            var payload = new Dictionary<string, string>
+            {
+                {"FirstName", $"{request.FirstName}"},
+                {"LastName", $"{request.LastName}"},
+                {"MiddleName", $"{request.MiddleName}"},
+                {"Email",$"{request.Email}"},
+                {"Password", $"{password}"},
+                {"PhoneNumber",$"{request.PhoneNumber}"},
+                {"Day",$"{request.Day}"},
+                {"Month",$"{request.Month}"},
+                {"Year",$"{request.Year}"}
+            };
+            string strPayload = JsonConvert.SerializeObject(payload);
+            HttpContent c = new StringContent(strPayload, Encoding.UTF8, "application/json");
+            using (var client = new HttpClient())
+            {
+                HttpRequestMessage AuthorRequest = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = u,
+                    Content = c
+                };
+                HttpResponseMessage result = await client.SendAsync(AuthorRequest, cancellationToken);
+                if (result.IsSuccessStatusCode)
+                {
+                    response = await result.Content.ReadAsStringAsync(cancellationToken);
+
+                }
+            }
+
+            return response;
+        }
+        public async Task<string> SendReceptionistInfoForRegistrationAsync(ReceptionistDTO request, CancellationToken cancellationToken)
+        {
+            var password = Guid.NewGuid().ToString().Substring(0, 8);
+            var response = string.Empty;
+            Uri u = new Uri("https://localhost:44336/api/User/registration");
+            var payload = new Dictionary<string, string>
+            {
+                {"FirstName", $"{request.FirstName}"},
+                {"LastName", $"{request.LastName}"},
+                {"MiddleName", $"{request.MiddleName}"},
+                {"Email",$"{request.Email}"},
+                {"Password", $"{password}"},
+                {"PhoneNumber",$"{request.PhoneNumber}"},
+                {"Day",$"{request.Day}"},
+                {"Month",$"{request.Month}"},
+                {"Year",$"{request.Year}"}
+            };
+            string strPayload = JsonConvert.SerializeObject(payload);
+            HttpContent c = new StringContent(strPayload, Encoding.UTF8, "application/json");
+            using (var client = new HttpClient())
+            {
+                HttpRequestMessage AuthorRequest = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = u,
+                    Content = c
+                };
+                HttpResponseMessage result = await client.SendAsync(AuthorRequest, cancellationToken);
+                if (result.IsSuccessStatusCode)
+                {
+                    response = await result.Content.ReadAsStringAsync(cancellationToken);
+
+                }
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Microservices/Profile/Profile.Application.Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Profile.Application.Services
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IAuthorizationService,AuthorizationService>();
+        }
+    }
+}
