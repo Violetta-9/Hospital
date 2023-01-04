5fa51773c53d066c8e9a69254d63b4486c0a44c5
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-04 16:50:45 +0300

    Command validators for doctor

---
+++ Microservices/Profile/Profile.Application/Validators/Commands/Doctor/DeleteDoctorValidator.cs
@@ -0,0 +1,34 @@
+﻿using Authorization.Data_Domain.Models;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Profile.Application.Command.Doctors.DeleteDoctor;
+using Profile.Application.Resources;
+
+namespace Profile.Application.Validators.Commands.Doctor
+{
+    public class DeleteDoctorValidator : AbstractValidator<DeleteDoctorCommand>
+    {
+        private readonly UserManager<Account> _userManager;
+
+        public DeleteDoctorValidator(UserManager<Account> userManager)
+        {
+            _userManager = userManager;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x=>x.AccountId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(Messages.EmptyField)
+                .MustAsync(ExistsAccountAsync)
+                .WithMessage(opt => string.Format(Messages.NotFoundAccount, opt.AccountId));
+        }
+        private async Task<bool> ExistsAccountAsync(string id, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            return user != null;
+        }
+    }
+}
---
+++ Microservices/Profile/Profile.Application/Validators/Commands/Doctor/UpdateDoctorProfileValidator.cs
@@ -0,0 +1,62 @@
+﻿using FluentValidation;
+using Profile.Application.Command.Doctors.UpdateDoctor.UpdateDoctorProfile;
+using Authorization.Data_Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Profile.Application.Resources;
+
+namespace Profile.Application.Validators.Commands.Doctor
+{
+    public class UpdateDoctorProfileValidator: AbstractValidator<UpdateDoctorProfileCommand>
+    {
+        private readonly UserManager<Account> _userManager;
+
+        public UpdateDoctorProfileValidator(UserManager<Account> userManager)
+        {
+            _userManager = userManager;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.DoctorInfo.AccountId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(Messages.EmptyField)
+                .MustAsync(ExistsAccountAsync)
+                .WithMessage(opt => string.Format(Messages.NotFoundAccount, opt.DoctorInfo.AccountId));
+            //todo: validation
+            //RuleFor(x=>x.DoctorInfo.OfficeId)
+            //    .Cascade(CascadeMode.Stop)
+            //    .NotEmpty()
+            //    .WithMessage(Messages.EmptyField)
+            //    .MustAsync(ExistsOfficeAsync)
+            //    .WithMessage(opt => string.Format(Messages.NotFoundOffice, opt.DoctorInfo.OfficeId));
+
+
+            //RuleFor(x => x.DoctorInfo.SpecializationId)
+            //    .Cascade(CascadeMode.Stop)
+            //    .NotEmpty()
+            //    .WithMessage(Messages.EmptyField)
+            //    .MustAsync(ExistsSpecializationAsync)
+            //    .WithMessage(opt => string.Format(Messages.NotFoundSpecialition, opt.DoctorInfo.SpecializationId));
+
+        }
+
+        //private Task<bool> ExistsSpecializationAsync(long specId, CancellationToken arg2)
+        //{
+
+        //}
+
+      
+        //private Task<bool> ExistsOfficeAsync(long officeId, CancellationToken arg2)
+        //{
+
+        //}
+
+        private async Task<bool> ExistsAccountAsync(string id, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            return user != null;
+        }
+    }
+}
