d8d7ecec32a6450dcac04c118acd9f6f822a6e11
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-05 22:34:31 +0300

    Update AuthorizationServer for using typed HttpClient

--- Microservices/Profile/Profile.Application.Services/AuthorizationService.cs
+++ Microservices/Profile/Profile.Application.Services/AuthorizationService.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using System.Globalization;
 using Newtonsoft.Json;
 using Profile.Application.Contracts.Incoming;
 using System.Text;
@@ -12,86 +13,71 @@ namespace Profile.Application.Services
         public Task<string> SendDoctorInfoForRegistrationAsync(DoctorDTO request, string password,
             CancellationToken cancellationToken);
 
-        public Task<string> SendReceptionistInfoForRegistrationAsync(ReceptionistDTO request,string password,
+        public Task<string> SendReceptionistInfoForRegistrationAsync(ReceptionistDTO request, string password,
             CancellationToken cancellationToken);
 
     }
 
-    public  class AuthorizationService:IAuthorizationService
+    public class AuthorizationService : IAuthorizationService
     {
+        private readonly HttpClient _httpClient;
+
+        public AuthorizationService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
         public async Task<string> SendDoctorInfoForRegistrationAsync(DoctorDTO request, string password, CancellationToken cancellationToken)
         {
-         
-            var response = string.Empty;
-            Uri u = new Uri("https://localhost:44336/api/User/registration");
+
             var payload = new Dictionary<string, string>
             {
-                {"FirstName", $"{request.FirstName}"},
-                {"LastName", $"{request.LastName}"},
-                {"MiddleName", $"{request.MiddleName}"},
-                {"Email",$"{request.Email}"},
-                {"Password", $"{password}"},
-                {"PhoneNumber",$"{request.PhoneNumber}"},
-                {"Day",$"{request.Day}"},
-                {"Month",$"{request.Month}"},
-                {"Year",$"{request.Year}"}
+                {"FirstName", request.FirstName},
+                {"LastName", request.LastName},
+                {"MiddleName", request.MiddleName},
+                {"Email",request.Email},
+                {"Password", password},
+                {"PhoneNumber",request.PhoneNumber},
+                {"BirthDate", request.BirthDate.ToString("s")}
+
             };
             string strPayload = JsonConvert.SerializeObject(payload);
-            HttpContent c = new StringContent(strPayload, Encoding.UTF8, "application/json");
-            using (var client = new HttpClient())
-            {
-                HttpRequestMessage AuthorRequest = new HttpRequestMessage
-                {
-                    Method = HttpMethod.Post,
-                    RequestUri = u,
-                    Content = c
-                };
-                HttpResponseMessage result = await client.SendAsync(AuthorRequest, cancellationToken);
-                if (result.IsSuccessStatusCode)
-                {
-                    response = await result.Content.ReadAsStringAsync(cancellationToken);
 
-                }
+            HttpContent c = new StringContent(strPayload, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("api/User/registration", c, cancellationToken);
+            var responseStream = await response.Content.ReadAsStringAsync(cancellationToken);
+            
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(responseStream);
             }
 
-            return response;
+            return responseStream;
         }
-        public async Task<string> SendReceptionistInfoForRegistrationAsync(ReceptionistDTO request,string password, CancellationToken cancellationToken)
+        public async Task<string> SendReceptionistInfoForRegistrationAsync(ReceptionistDTO request, string password, CancellationToken cancellationToken)
         {
-          
-            var response = string.Empty;
-            Uri u = new Uri("https://localhost:44336/api/User/registration");
+
+            
             var payload = new Dictionary<string, string>
             {
-                {"FirstName", $"{request.FirstName}"},
-                {"LastName", $"{request.LastName}"},
-                {"MiddleName", $"{request.MiddleName}"},
-                {"Email",$"{request.Email}"},
-                {"Password", $"{password}"},
-                {"PhoneNumber",$"{request.PhoneNumber}"},
-                {"Day",$"{request.Day}"},
-                {"Month",$"{request.Month}"},
-                {"Year",$"{request.Year}"}
+                {"FirstName", request.FirstName},
+                {"LastName", request.LastName},
+                {"MiddleName", request.MiddleName},
+                {"Email",request.Email},
+                {"Password", password},
+                {"PhoneNumber",request.PhoneNumber},
+                {"BirthDate", request.BirthDate.ToString("s")}
             };
             string strPayload = JsonConvert.SerializeObject(payload);
             HttpContent c = new StringContent(strPayload, Encoding.UTF8, "application/json");
-            using (var client = new HttpClient())
-            {
-                HttpRequestMessage AuthorRequest = new HttpRequestMessage
-                {
-                    Method = HttpMethod.Post,
-                    RequestUri = u,
-                    Content = c
-                };
-                HttpResponseMessage result = await client.SendAsync(AuthorRequest, cancellationToken);
-                if (result.IsSuccessStatusCode)
-                {
-                    response = await result.Content.ReadAsStringAsync(cancellationToken);
+            var response = await _httpClient.PostAsync("api/User/registration", c, cancellationToken);
+            var responseStream = await response.Content.ReadAsStringAsync(cancellationToken);
 
-                }
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(responseStream);
             }
 
-            return response;
+            return responseStream;
         }
     }
 }
