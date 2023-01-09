cdb0973d6e09cca7f8cd5caff8c3e81503e7ee02
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-09 12:01:35 +0300

    Create Validators for confirm Email

---
+++ Microservices/Authorization/Authorization.Application/Validators/Command/ConfirmEmailValidator.cs
@@ -0,0 +1,44 @@
+﻿using Authorization.Application.Command.User.Registration;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Application.Command.User.ConfirmEmail;
+using Authorization.Application.Resources;
+using Authorization.Data_Domain.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authorization.Application.Validators.Command
+{
+    internal class ConfirmEmailValidator: AbstractValidator<ConfirmEmailCommand>
+    {
+        private readonly UserManager<Account> _userManager;
+        public ConfirmEmailValidator(UserManager<Account> userManager)
+        {
+            _userManager = userManager;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.UserId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(Messages.NotEmtyField)
+                .MustAsync(ExistsAccountAsync)
+                .WithMessage(opt => string.Format(Messages.NotFoundUser, opt.UserId));
+
+            RuleFor(x => x.Token)
+                .NotEmpty()
+                .WithMessage(Messages.NotEmptyToken);
+        }
+
+        private async Task<bool> ExistsAccountAsync(string id, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            return user != null;
+        }
+    }
+}
