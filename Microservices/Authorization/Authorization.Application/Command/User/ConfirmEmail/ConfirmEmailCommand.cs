716443aadcebdbfb5cf989ff4de731f0e7ea216c
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-09 12:00:46 +0300

    Create CorfimEmailCommand

---
+++ Microservices/Authorization/Authorization.Application/Command/User/ConfirmEmail/ConfirmEmailCommand.cs
@@ -0,0 +1,17 @@
+﻿using Authorization.Application.Contracts.Outgoing;
+using MediatR;
+
+
+namespace Authorization.Application.Command.User.ConfirmEmail
+{
+    public  class ConfirmEmailCommand:IRequest<Response>
+    {
+        public string UserId { get; set; }
+        public string Token { get; set; }
+        public ConfirmEmailCommand(string userId, string token)
+        {
+           UserId = userId;
+            Token= token;
+        }
+    }
+}
