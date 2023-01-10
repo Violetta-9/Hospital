b6e3209171727ad7c35433db6e1ad6654c38157a
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-10 14:10:37 +0300

    CreateOffice command

---
+++ Microservices/Office/Office/Office.Application/Command/CreateOffice/CreateOfficeCommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Office.Application.Contracts.Incoming;
+
+namespace Office.Application.Command.CreateOffice
+{
+    public class CreateOfficeCommand : IRequest<long>
+    {
+        public CreateOfficeDTO OfficeDto { get; set; }
+
+        public CreateOfficeCommand(CreateOfficeDTO officeDto)
+        {
+            OfficeDto=officeDto;
+        }
+    }
+}
