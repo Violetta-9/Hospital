b5c3c2b721fff706f6e79687a41c5c1e890c8b6a
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-10 14:08:09 +0300

    CreateDto for office

---
+++ Microservices/Office/Office/Office.Application.Contracts/Incoming/CreateOfficeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Office.Application.Contracts.Incoming
+{
+    public class CreateOfficeDTO
+    {
+        public string Address { get; set; }
+        public string RegistryPhoneNumber { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Microservices/Office/Office/Office.Application.Contracts/Incoming/UpdateOfficeStatusDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Office.Application.Contracts.Incoming
+{
+    public class UpdateOfficeStatusDTO
+    {
+        public long OfficeId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
