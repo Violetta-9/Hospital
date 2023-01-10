a8c5db4626a443ab7459cb00372c241f6e906c29
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-10 14:11:02 +0300

    Update office command

---
+++ Microservices/Office/Office/Office.Application/Command/UpdateOffice/UpdateOfficeCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Office.Application.Contracts.Incoming;
+using Office.Application.Contracts.Outgoing;
+
+namespace Office.Application.Command.UpdateOffice
+{
+    public class UpdateOfficeCommand:IRequest<Response>
+    {
+     
+        public UpdateOfficeDTO UpdateOfficeDto { get; set; }
+        public UpdateOfficeCommand(UpdateOfficeDTO updateOfficeDto)
+        {
+
+            UpdateOfficeDto = updateOfficeDto;
+        }
+    }
+}
