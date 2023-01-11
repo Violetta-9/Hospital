52c71fd5c153f346fa971342b9a7b2f96d5a8f90
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-11 17:08:47 +0300

    Add new entity and update spezialization entity

---
+++ Microservices/Shared/Authorization.Data.Domain/Models/ServiceCategory.cs
@@ -0,0 +1,16 @@
+﻿using Authorization.Data_Domain.Models.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization.Data_Domain.Models
+{
+    public class ServiceCategory: KeyedEntityBase
+    {
+        public string Title { get; set; }
+        public double TimeSlotSize { get; set; }
+        public virtual ICollection<Service> Services { get; set; }
+    }
+}
--- Microservices/Shared/Authorization.Data.Domain/Models/Specialization.cs
+++ Microservices/Shared/Authorization.Data.Domain/Models/Specialization.cs
@@ -7,4 +7,5 @@ public class Specialization : KeyedEntityBase
     public string Title { get; set; }
     public bool IsActive { get; set; }
     public virtual ICollection<Doctor> Doctors { get; set; }
+    public virtual ICollection<Service> Services { get; set; }
 }
