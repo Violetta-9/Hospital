b880537f96a76760e8e38b2ed954a7d4e73fdc47
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-11 17:13:19 +0300

    Validators

---
+++ Microservices/Services.API/Services.API.Application/Validator/Command/CreateServiceValidator.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidation;
+using Services.API.Application.Command.UpdateService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.API.Application.Command.CreateService;
+using Authorization.Data.Repository;
+using Services.API.Resources;
+
+namespace Services.API.Application.Validator.Command
+{
+    public class CreateServiceValidator: AbstractValidator<CreateServiceCommand>
+    {
+        private readonly IServiceRepository _serviceRepository;
+        private readonly IServiceCategoryRepository _categoryRepository;
+
+        public CreateServiceValidator(IServiceRepository serviceRepository, IServiceCategoryRepository serviceCategory)
+        {
+            _serviceRepository = serviceRepository;
+            _categoryRepository = serviceCategory;
+            CreateRules();
+        }
+        private void CreateRules()
+        {
+           
+            RuleFor(x => x.CreateServiceDto.Price)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(opt => string.Format(Messages.NotEmptyField, nameof(opt.CreateServiceDto.Price)))
+                .Must(CorrectPrice)
+                .WithMessage(Messages.InCorrectPrice);
+            RuleFor(x => x.CreateServiceDto.ServiceCategoryId)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(opt =>
+                    string.Format(Messages.NotEmptyField, nameof(opt.CreateServiceDto.ServiceCategoryId)))
+                .MustAsync(ExistsServiceCategory)
+                .WithMessage(opt =>
+                    string.Format(Messages.NotFoundServiceCategory, opt.CreateServiceDto.ServiceCategoryId));
+
+        }
+
+        private async Task<bool> ExistsServiceCategory(long categoryId, CancellationToken cancellationToken)
+        {
+            return await _categoryRepository.ExistsAsync(categoryId, cancellationToken);
+        }
+
+        private bool CorrectPrice(double price)
+        {
+            return price > 0;
+        }
+
+       
+    }
+}
---
+++ Microservices/Services.API/Services.API.Application/Validator/Command/UpdateServiceStatusValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.API.Application.Command.UpdateServiceStatus;
+using Authorization.Data.Repository;
+using Services.API.Resources;
+
+namespace Services.API.Application.Validator.Command
+{
+    public class UpdateServiceStatusValidator: AbstractValidator<UpdateServiceStatusCommand>
+    {
+        private readonly IServiceRepository _serviceRepository;
+
+        public UpdateServiceStatusValidator(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.UpdateServiceStatusDTO.Id)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(opt => string.Format(Messages.NotEmptyField, nameof(opt.UpdateServiceStatusDTO.Id)))
+                .MustAsync(ExistsServiceAsync)
+                .WithMessage(opt => string.Format(Messages.NotFoundService, opt.UpdateServiceStatusDTO.Id));
+
+            RuleFor(x => x.UpdateServiceStatusDTO.IsActive)
+                .NotEmpty()
+                .WithMessage(opt=>string.Format(Messages.NotEmptyField,nameof(opt.UpdateServiceStatusDTO.IsActive)));
+        }
+
+        private async Task<bool> ExistsServiceAsync(long serviceId, CancellationToken cancellationToken)
+        {
+            return await _serviceRepository.ExistsAsync(serviceId, cancellationToken);
+        }
+    }
+}
