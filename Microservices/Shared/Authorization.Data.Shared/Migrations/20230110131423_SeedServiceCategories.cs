36448c4cfacc4a5392e3682cfc959e4d29a27dab
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-11 17:11:10 +0300

    Migration

---
+++ Microservices/Shared/Authorization.Data.Shared/Migrations/20230110131423_SeedServiceCategories.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Authorization.Data.Shared.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedServiceCategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"INSERT INTO ""ServiceCategories"" (""Title"",""TimeSlotSize"", ""RowCreatedTimestamp"",""LastRowModificationTimestamp"") VALUES('analyses',1,NOW(),NOW())");
+            migrationBuilder.Sql(@"INSERT INTO ""ServiceCategories"" (""Title"",""TimeSlotSize"", ""RowCreatedTimestamp"",""LastRowModificationTimestamp"") VALUES('consultation',1,NOW(),NOW())");
+            migrationBuilder.Sql(@"INSERT INTO ""ServiceCategories"" (""Title"",""TimeSlotSize"", ""RowCreatedTimestamp"",""LastRowModificationTimestamp"") VALUES('diagnostics',1.5,NOW(),NOW())");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
