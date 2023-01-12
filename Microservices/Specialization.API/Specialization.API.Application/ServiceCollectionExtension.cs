0c95bcd0fd0e2ff91bb0e3133157c0725d726c3d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 18:34:06 +0300

    Update Application

---
+++ Microservices/Specialization.API/Specialization.API.Application/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Specialization.API.Application.PipelineBehaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specialization.API.Application
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            //services.AddScoped<IValidator<UpdateOfficeCommand>, UpdateOfficeValidator>(); ;
+
+
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            AssemblyScanner.FindValidatorsInAssembly(typeof(ServiceCollectionExtension).Assembly)
+                .ForEach(item => services.AddScoped(item.InterfaceType, item.ValidatorType));
+        }
+    }
+}
