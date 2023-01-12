595896b732fc8cf5daa5630b1aa84e2a27d0ff69
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 18:35:24 +0300

    Helpers

---
+++ Microservices/Specialization.API/Specialization.API.Application.Helpers/UriSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specialization.API.Application.Helpers
+{
+    public class UriSettings
+    {
+        public string BasedAddress { get; set; }
+        public string RegistrationPath { get; set; }
+    }
+}
