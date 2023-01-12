1341af87021f22cb4e707850bf523b699d7bd40f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 18:35:15 +0300

    Create Services

---
+++ Microservices/Specialization.API/Specialization.API.Application.Services/ServicesService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using Specialization.API.Application.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specialization.API.Application.Services
+{
+    public interface IServicesService
+    {
+
+    }
+
+    public class ServicesService : IServicesService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly UriSettings _uriSettings;
+        public ServicesService(HttpClient httpClient, IOptions<UriSettings> uriSettings)
+        {
+            _httpClient = httpClient;
+            _uriSettings = uriSettings.Value;
+        }
+    }
+}
