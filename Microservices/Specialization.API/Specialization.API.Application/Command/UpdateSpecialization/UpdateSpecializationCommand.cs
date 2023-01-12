5252ae1c9faafaac65383854714f4a7ce5abb9d4
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 18:32:11 +0300

    Update command

---
+++ Microservices/Specialization.API/Specialization.API.Application/Command/UpdateSpecialization/UpdateSpecializationCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Specialization.API.Application.Contracts.Incoming;
+using Specialization.API.Application.Contracts.Outgoing;
+
+namespace Specialization.API.Application.Command.UpdateSpecialization
+{
+    public class UpdateSpecializationCommand:IRequest<Response>
+    {
+        public long Id { get; set; }
+        public ICollection<long> ServicesId { get; set; }
+        public string Title { get; set; }
+
+        public UpdateSpecializationCommand(UpdateSpecializationDTO updateSpecializationDto)
+        {
+            Id=updateSpecializationDto.Id;
+            Title=updateSpecializationDto.Title;
+                ServicesId=updateSpecializationDto.ServicesId;
+        }
+    }
+}
---
+++ Microservices/Specialization.API/Specialization.API.Application/Command/UpdateSpecialization/UpdateSpecializationCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Specialization.API.Application.Contracts.Outgoing;
+
+namespace Specialization.API.Application.Command.UpdateSpecialization
+{
+    public class UpdateSpecializationCommandHandler : IRequestHandler<UpdateSpecializationCommand, Response>
+    {
+        private readonly ISpecializationRepository _specializationRepository;
+        private readonly IServiceRepository _serviceRepository;
+
+        public UpdateSpecializationCommandHandler(IServiceRepository serviceRepository,
+            ISpecializationRepository specializationRepository)
+        {
+            _serviceRepository= serviceRepository;
+            _specializationRepository= specializationRepository;
+        }
+        public async Task<Response> Handle(UpdateSpecializationCommand request, CancellationToken cancellationToken)
+        {
+           var specialization=await _specializationRepository.GetAsync(request.Id, cancellationToken);
+           if (specialization == null)
+           {
+                return Response.Error;
+           }
+           specialization.Title=request.Title;
+          await _specializationRepository.UpdateAsync(specialization, cancellationToken);
+         await _serviceRepository.SetSpecializationAsync(request.ServicesId, specialization.Id, cancellationToken);
+         return Response.Success;
+        }
+    }
+}
