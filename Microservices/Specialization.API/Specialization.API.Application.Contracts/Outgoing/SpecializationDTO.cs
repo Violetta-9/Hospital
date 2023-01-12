6c62e4b32f612e566dbd20e8d6b466f8e36f5a5d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 18:34:36 +0300

    Outgoing contracts

---
+++ Microservices/Specialization.API/Specialization.API.Application.Contracts/Outgoing/SpecializationDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specialization.API.Application.Contracts.Outgoing
+{
+    public class SpecializationDTO
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+        public ICollection<OutServicesDto> Services { get; set; }
+    }
+}
---
+++ Microservices/Specialization.API/Specialization.API.Application.Contracts/Outgoing/SpecializationListDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specialization.API.Application.Contracts.Outgoing
+{
+    public class SpecializationListDTO
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
