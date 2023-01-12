edc637a4abfbe5d2aa904f2c4ccafd134bcb0b4f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 19:13:35 +0300

    Set specialization command

---
+++ Microservices/Services.API/Services.API.Application/Command/SetSpecializationForService/SetSpecializationCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Services.API.Contracts.Outgoing;
+
+namespace Services.API.Application.Command.SetSpecializationForService
+{
+    internal class SetSpecializationCommandHandler : IRequestHandler<SetSpecializationCommand, Response>
+    {
+        private readonly IServiceRepository _serviceRepository;
+
+        public SetSpecializationCommandHandler(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+        }
+        public async Task<Response> Handle(SetSpecializationCommand request, CancellationToken cancellationToken)
+        {
+            await _serviceRepository.SetSpecializationAsync(request.SetSpecializationDTO.ServicesId,request.SetSpecializationDTO.SpecializationId,
+                cancellationToken);
+            return Response.Success;
+        }
+    }
+}
