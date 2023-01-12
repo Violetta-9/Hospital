0755c4e2377cd56b7f626f9485f4ff04cd65c91c
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 18:33:04 +0300

    Validators command

---
+++ Microservices/Specialization.API/Specialization.API.Application/Validator/Command/CreateSpecializationValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using Specialization.API.Application.Command.CreateSpecialization;
+using Specialization.API.Application.Resources;
+
+namespace Specialization.API.Application.Validator.Command
+{
+    public class CreateSpecializationValidator:AbstractValidator<CreateSpecializationCommand>
+    {
+        public CreateSpecializationValidator()
+        {
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.CreateSpecializationDto.Title)
+                .NotEmpty()
+                .WithMessage(opt=>string.Format(Messages.NotEmptyField,nameof(opt.CreateSpecializationDto.Title)));
+
+            RuleFor(x => x.CreateSpecializationDto.IsActive)
+                .NotEmpty()
+                .WithMessage(opt => string.Format(Messages.NotEmptyField, nameof(opt.CreateSpecializationDto.IsActive)));
+           
+        }
+    }
+}
---
+++ Microservices/Specialization.API/Specialization.API.Application/Validator/Command/UpdateSpecializationValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using FluentValidation;
+using Specialization.API.Application.Command.UpdateSpecialization;
+using Specialization.API.Application.Resources;
+
+namespace Specialization.API.Application.Validator.Command
+{
+    public class UpdateSpecializationValidator:AbstractValidator<UpdateSpecializationCommand>
+    {
+        private readonly ISpecializationRepository _specializationRepository;
+        public UpdateSpecializationValidator(ISpecializationRepository specializationRepository)
+        {
+            _specializationRepository = specializationRepository;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.Id)
+                .Cascade(CascadeMode.Stop)
+                .NotEmpty()
+                .WithMessage(opt => string.Format(Messages.NotEmptyField, nameof(opt.Id)))
+                .MustAsync(SpecializationExist)
+                .WithMessage(opt => string.Format(Messages.NotFoundSpecialization, opt.Id));
+
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .WithMessage(opt => string.Format(Messages.NotEmptyField, nameof(opt.Title)));
+        }
+
+        private async Task<bool> SpecializationExist(long id, CancellationToken cancellationToken)
+        {
+            return await _specializationRepository.ExistsAsync(id, cancellationToken);
+        }
+    }
+}
