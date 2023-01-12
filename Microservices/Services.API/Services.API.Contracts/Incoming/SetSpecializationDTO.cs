0ed4908af4120813cdbbbfc4b33c67de423ba332
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 19:14:02 +0300

    New dto

---
+++ Microservices/Services.API/Services.API.Contracts/Incoming/SetSpecializationDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.API.Contracts.Incoming
+{
+    public class SetSpecializationDTO
+    {
+        public long SpecializationId { get; set; }
+        public virtual ICollection<long> ServicesId { get; set; }
+    }
+}
