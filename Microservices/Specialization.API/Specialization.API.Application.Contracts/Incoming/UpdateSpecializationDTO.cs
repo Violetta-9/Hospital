bf98842d78276e4e5efee7b386d7efa983ac011a
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-12 18:34:24 +0300

    Incoming contracts

---
+++ Microservices/Specialization.API/Specialization.API.Application.Contracts/Incoming/UpdateSpecializationDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specialization.API.Application.Contracts.Incoming
+{
+    public class UpdateSpecializationDTO
+    {
+        public long Id { get; set; }
+        public ICollection<long> ServicesId { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Microservices/Specialization.API/Specialization.API.Application.Contracts/Incoming/UpdateSpecializationStatusDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specialization.API.Application.Contracts.Incoming
+{
+    public class UpdateSpecializationStatusDTO
+    {
+        public long Id { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
