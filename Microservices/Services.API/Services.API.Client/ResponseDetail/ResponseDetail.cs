8ab82305486da0ad246bacba9f0dc8b0bfcb41d9
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-17 13:59:46 +0300

    ResponceDetail for processing exception

---
+++ Microservices/Services.API/Services.API.Client/ResponseDetail/ResponseDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.API.Client.ResponseDetail
+{
+    public class ResponseDetail
+    {
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public int Status { get; set; }
+        public string Detail { get; set; }
+    }
+}
