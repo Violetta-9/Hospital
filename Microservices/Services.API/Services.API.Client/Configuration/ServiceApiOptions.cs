2583233918d8294416e15126ab3416883d87bd49
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-17 13:58:05 +0300

    Configuration

---
+++ Microservices/Services.API/Services.API.Client/Configuration/ServiceApiOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.API.Client.Configuration
+{
+    public class ServiceApiOptions
+    {
+        public string ServiceUrl { get; set; }
+    
+    }
+}
