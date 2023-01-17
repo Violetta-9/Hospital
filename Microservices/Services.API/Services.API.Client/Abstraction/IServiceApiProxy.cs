e0209a05404e9ba397f6fb97d227e6f6326ae011
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-17 13:57:47 +0300

    ApiProxy and interface for it

---
+++ Microservices/Services.API/Services.API.Client/Abstraction/IServiceApiProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Services.API.Client.GeneratedClient;
+
+namespace Services.API.Client.Abstraction
+{
+    public interface IServiceApiProxy
+    {
+        public Task<Response> SetSpecializationIdForServicesAsync(long specializationId, ICollection<long> servicesId,
+            CancellationToken cancellationToken);
+    }
+}
---
+++ Microservices/Services.API/Services.API.Client/ServiceApiProxy.cs
@@ -0,0 +1,57 @@
+﻿
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Services.API.Client.Abstraction;
+using Services.API.Client.Configuration;
+using Services.API.Client.GeneratedClient;
+using Services.API.Client.HttpClientProvider.Abstraction;
+
+namespace Services.API.Client
+{
+    public class ServiceApiProxy : IServiceApiProxy
+    {
+        private string BaseUrl { get; set; }
+        private readonly IServiceApiHttpClientProvider _httpClientProvider;
+        public ServiceApiProxy(IServiceApiHttpClientProvider httpClientProvider, IOptions<ServiceApiOptions> serviceOptions)
+        {
+            BaseUrl = serviceOptions.Value.ServiceUrl;
+            _httpClientProvider = httpClientProvider;
+        }
+
+        public async Task<Response> SetSpecializationIdForServicesAsync(long specializationId,
+            ICollection<long> servicesId, CancellationToken cancellationToken)
+        {
+            var api = await GetApiClientAsync(cancellationToken);
+            try
+            {
+                var response = await api.SetSpecializationForServiceAsync(new SetSpecializationDTO()
+                {
+                    ServicesId = servicesId,
+                    SpecializationId = specializationId
+                }, cancellationToken);
+                return response;
+            }
+            catch (ApiException e)
+            {
+                var error = JsonConvert.DeserializeObject<ResponseDetail.ResponseDetail>(e.Response);
+                    throw new ValidationException(new List<ValidationFailure>()
+                    {
+                        new(string.Empty, error?.Detail)
+                    });
+                
+               
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public async Task<ServiceApi> GetApiClientAsync(CancellationToken cancellationToken = default)
+        {
+            return new ServiceApi(BaseUrl, await _httpClientProvider.GetHttpClientAsync(cancellationToken));
+        }
+    }
+}
