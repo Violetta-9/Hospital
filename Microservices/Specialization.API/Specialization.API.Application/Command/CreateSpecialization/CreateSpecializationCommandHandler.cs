b7582d526a355472d97ad26ebac2f6a32cb63d7f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-17 14:16:33 +0300

    Update CreateSpecializationCommandHandler

--- Microservices/Specialization.API/Specialization.API.Application/Command/CreateSpecialization/CreateSpecializationCommandHandler.cs
+++ Microservices/Specialization.API/Specialization.API.Application/Command/CreateSpecialization/CreateSpecializationCommandHandler.cs
@@ -6,9 +6,12 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using Authorization.Data.Repository;
+using Authorization.Data_Domain.Models;
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Http;
-using Specialization.API.Application.Services;
+using Services.API.Client.Abstraction;
+
 using SpecializationEntity= Authorization.Data_Domain.Models.Specialization;
 
 namespace Specialization.API.Application.Command.CreateSpecialization
@@ -16,14 +19,12 @@ namespace Specialization.API.Application.Command.CreateSpecialization
     internal class CreateSpecializationCommandHandler : IRequestHandler<CreateSpecializationCommand, long>
     {
         private readonly ISpecializationRepository _specializationRepository;
-        private readonly IServicesService _servicesService;
-      
-        
+        private readonly IServiceApiProxy _serviceApiProxy;
 
-        public CreateSpecializationCommandHandler(ISpecializationRepository specializationRepository,IServicesService servicesService)
+        public CreateSpecializationCommandHandler(ISpecializationRepository specializationRepository,IServiceApiProxy serviceApiProxy)
         {
             _specializationRepository = specializationRepository;
-            _servicesService = servicesService;
+            _serviceApiProxy = serviceApiProxy;
          
             
         }
@@ -33,20 +34,33 @@ namespace Specialization.API.Application.Command.CreateSpecialization
             var specialization = new SpecializationEntity()
             {
                 Title = request.CreateSpecializationDto.Title,
-                IsActive = request.CreateSpecializationDto.IsActive,
-
+                IsActive = request.CreateSpecializationDto.IsActive
             };
 
             
             await _specializationRepository.InsertAsync(specialization, cancellationToken);
-            var serviceResponse = await _servicesService.SetSpecializationIdForServicesAsync(specialization.Id,
-                request.CreateSpecializationDto.ServicesId, cancellationToken);
+            try
+            {
+                var serviceResponse = await _serviceApiProxy.SetSpecializationIdForServicesAsync(specialization.Id,
+                    request.CreateSpecializationDto.ServicesId, cancellationToken);
+                if (!serviceResponse.IsSuccess)
+                {
+                    throw new Exception("CreateSpecializationCommandHandler work bad");
 
-         if (!serviceResponse.IsSuccess)
-         {
-             throw new Exception("CreateSpecializationCommandHandler work bad");
-         } 
-         return specialization.Id;
+                }
+            }
+            catch (ValidationException e)
+            {
+                await _specializationRepository.DeleteAsync(specialization.Id, cancellationToken);
+                throw e;
+            }
+            catch (Exception e)
+            {
+                await _specializationRepository.DeleteAsync(specialization.Id, cancellationToken);
+                throw e;
+            }
+
+            return specialization.Id;
 
         }
     }
