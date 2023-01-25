aa8b3afd70e40b1a39d2a6c6a5d332a6c5a4c2ec
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-25 13:24:23 +0300

    Create new common type

---
+++ Microservices/SharedTypeForRabbitmq/SpecializationStatusChanged/SpecializationStatusChanged/SpecializationStatusChanged/SpecializationStatusChanged.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecializationStatusChanged
+{
+    public class SpecializationStatusChanged
+    {
+        public long SpecializationId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
