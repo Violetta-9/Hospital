2abd842fac1bc22e9214250d0eaaefc40afa767d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-29 14:02:31 +0300

    Helper for documents

---
+++ Microservices/Documents.API/Documents.API/Documents.API.Application.Helpers/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents.API.Application.Helpers
+{
+    public class UserRole
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+        public const string Doctor = "Doctor";
+        public const string Patient = "Patient";
+        public const string Receptionist = "Receptionist";
+    }
+}
