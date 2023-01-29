7f474c6465f622f139e0d70b52b2a4d8467aaaf9
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-29 13:55:04 +0300

    Create Document entity

---
+++ Microservices/Shared/Authorization.Data.Domain/Models/Documentation.cs
@@ -0,0 +1,20 @@
+﻿using Authorization.Data_Domain.Models.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authorization.Data_Domain.Models
+{
+    public class Documentation: KeyedEntityBase
+    {
+        public string FileName { get; set; }
+        public string Path { get; set; }
+        public string ContainerName { get; set; }
+        public virtual Office Office { get; set; }
+        public virtual Account Account { get; set; }
+     
+
+    }
+}
