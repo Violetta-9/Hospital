0ad35b5c6cd0861d54bd7bfcdc07de6e1a8f53d0
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-31 19:46:29 +0300

    ServiceExtension document

---
+++ Microservices/Documents.API/Documents.API/Documents.API.Client/ServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Documents.API.Client.Abstraction;
+using Documents.API.Client.Configuration;
+using Documents.API.Client.HttpClientProvider;
+using Documents.API.Client.HttpClientProvider.Abstraction;
+
+namespace Documents.API.Client
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddDocumentsApi(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.Configure<DocumentApiOptions>(configuration.GetSection("DocumentApi"));
+            service.AddScoped<IDocumentApiProxy, DocumentApiProxy>();
+            service.AddScoped<IDocumentApiHttpClientProvider, DefaultDocumentApiHttpClientProvider>();
+            return service;
+        }
+
+       
+    }
+}
