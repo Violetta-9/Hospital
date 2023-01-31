ee5a7400f8da03f4d6e387108c8d24a8fbc27e21
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-31 19:49:06 +0300

    Create BlobUrlHelper (for getting patch)

---
+++ Microservices/Profile/Profile.Application.Helpers/BlobUrlHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Profile.Application.Helpers
+{
+    public class BlobUrlHelpers
+    {
+        public string AbsolutUrl { get; set; }
+    }
+}
+
