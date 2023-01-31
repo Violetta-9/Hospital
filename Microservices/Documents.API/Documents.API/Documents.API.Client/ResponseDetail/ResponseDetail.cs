bf1bfbd0af3369918f7b46096702cd312a3f79d4
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-31 19:45:59 +0300

    Response(exeption)

---
+++ Microservices/Documents.API/Documents.API/Documents.API.Client/ResponseDetail/ResponseDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Documents.API.Client.ResponseDetail
+{
+    public class ResponseDetail
+    {
+      
+            public string Title { get; set; }
+            public string Type { get; set; }
+            public int Status { get; set; }
+            public string Detail { get; set; }
+        
+    }
+}
