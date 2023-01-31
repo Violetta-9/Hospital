3c14e72546106c209ab753c09ac4a4b9e215548f
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-01-31 19:43:55 +0300

    Abstraction of apiProxy with implementation

---
+++ Microservices/Documents.API/Documents.API/Documents.API.Client/Abstraction/IDocumentApiProxy.cs
@@ -0,0 +1,14 @@
+﻿
+using Documents.API.Client.GeneratedClient;
+using Microsoft.AspNetCore.Http;
+
+namespace Documents.API.Client.Abstraction
+{
+    public interface IDocumentApiProxy
+    {
+        public Task<long> UploadBlobAsync(FileParameter file,long entityId,SubjectUpdate subjectUpdate,CancellationToken cancellationToken);
+        public Task<Response> DeleteBlobAsync(long documentId,CancellationToken cancellationToken);
+        public Task<BlobDTO> GetBlobAsync(long documentId,CancellationToken cancellationToken);
+    
+    }
+}
