89a6f25d5bf2b1ed056e7c6e91e398594c2e7ca8
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-06 15:44:25 +0300

    Add document,result,appoitment mapping configuration

---
+++ Microservices/Shared/Authorization.Data.Shared/MappingConfiguration/PhotoMappingConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Authorization.Data.Shared.MappingConfiguration.Extension;
+using Authorization.Data_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Authorization.Data.Shared.MappingConfiguration;
+
+public class PhotoMappingConfiguration : IEntityTypeConfiguration<Photo>
+{
+    public void Configure(EntityTypeBuilder<Photo> builder)
+    {
+        builder.BasedMapping();
+        builder.Property(x => x.Path).HasColumnName(nameof(Photo.Path)).HasMaxLength(512).IsRequired();
+        builder.Property(x => x.FileName).HasColumnName(nameof(Photo.FileName)).HasMaxLength(264).IsRequired();
+        builder.Property(x => x.ContainerName).HasColumnName(nameof(Photo.ContainerName)).HasMaxLength(100)
+            .IsRequired();
+        builder.HasOne(x => x.Account).WithOne(x => x.Photo).HasForeignKey<Account>(x => x.PhotoId);
+        builder.HasOne(x => x.Office).WithOne(x => x.Photo).HasForeignKey<Office>(x => x.PhotoId);
+    }
+}
---
+++ Microservices/Shared/Authorization.Data.Shared/MappingConfiguration/ResultMappingConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Authorization.Data_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Shared.MappingConfiguration.Extension;
+
+namespace Authorization.Data.Shared.MappingConfiguration
+{
+    public class ResultMappingConfiguration : IEntityTypeConfiguration<Result>
+    {
+        public void Configure(EntityTypeBuilder<Result> builder)
+        {
+            builder.BasedMapping();
+            builder.Property(x => x.Complaints).HasColumnName(nameof(Result.Complaints)).HasMaxLength(150).IsRequired();
+            builder.Property(x => x.Conclusion).HasColumnName(nameof(Result.Conclusion)).HasMaxLength(250).IsRequired();
+            builder.Property(x => x.Recomendations).HasColumnName(nameof(Result.Recomendations)).HasMaxLength(250).IsRequired();
+            builder.HasOne(x=>x.Appointment).WithOne(x=>x.Result).HasForeignKey<Result>(x=>x.AppointmentId);
+            
+        }
+    }
+}
