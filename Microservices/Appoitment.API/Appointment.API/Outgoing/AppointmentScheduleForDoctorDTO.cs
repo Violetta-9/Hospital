8fe9d9f1424feec8b42a819afd8533daabf4658b
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-09 21:27:17 +0300

    Contracts

---
+++ Microservices/Appoitment.API/Appointment.API/Outgoing/AppointmentScheduleForDoctorDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment.API.Application.Contracts.Outgoing
+{
+    public class AppointmentScheduleForDoctorDTO
+    {
+        public long AppointmentId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string PatientFullName { get; set; }
+        public string ServiceName { get; set; }
+        public bool ApprovedStatus { get; set; }
+    }
+}
---
+++ Microservices/Appoitment.API/Appointment.API/Outgoing/AppointmentScheduleForReceptionistDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appointment.API.Application.Contracts.Outgoing
+{
+    public class AppointmentScheduleForReceptionistDTO
+    {
+        public long AppointmentId { get; set; }
+        public DateTime DateTime { get; set; }
+        public string PatientFullName { get; set; }
+        public string DoctorFullName { get; set; }
+        public string PatientPhoneNumber { get; set; }
+        public string ServiceName { get; set; }
+
+    }
+}
