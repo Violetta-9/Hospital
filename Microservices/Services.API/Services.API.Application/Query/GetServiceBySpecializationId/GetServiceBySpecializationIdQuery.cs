8dd7b44b0a35c52b3fbff26b7024e5f67d8049e6
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-13 19:20:15 +0300

    Get service by specialization id

---
+++ Microservices/Services.API/Services.API.Application/Query/GetServiceBySpecializationId/GetServiceBySpecializationIdQuery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Services.API.Contracts.Outgoing;
+
+namespace Services.API.Application.Query.GetServiceBySpecializationId
+{
+    public class GetServiceBySpecializationIdQuery:IRequest<OutServicesDto[]>
+    {
+        public long SpecializationId { get; set; }
+
+        public GetServiceBySpecializationIdQuery(long specializationId)
+        {
+            SpecializationId=specializationId;
+        }
+    }
+}
---
+++ Microservices/Services.API/Services.API.Application/Query/GetServiceBySpecializationId/GetServiceBySpecializationIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Services.API.Contracts.Outgoing;
+
+namespace Services.API.Application.Query.GetServiceBySpecializationId
+{
+    internal class GetServiceBySpecializationIdQueryHandler : IRequestHandler<GetServiceBySpecializationIdQuery, OutServicesDto[]>
+    {
+
+        private readonly IServiceRepository _serviceRepository;
+
+        public GetServiceBySpecializationIdQueryHandler(IServiceRepository serviceRepository)
+        {
+            _serviceRepository = serviceRepository;
+        }
+        public async Task<OutServicesDto[]> Handle(GetServiceBySpecializationIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _serviceRepository.GetServiceBySpecializationIdAsync(request.SpecializationId,
+                cancellationToken);
+        }
+    }
+}
