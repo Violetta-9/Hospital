6b3ab8a2be66e0f0126589153048cdce834d03e9
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-13 18:15:07 +0300

    new repository (result)

---
+++ Microservices/Shared/Authorization.Data.Repository/ResultRepository.cs
@@ -0,0 +1,24 @@
+﻿using Appointment.API.Application.Contracts.Outgoing;
+using Authorization.Data.Repository.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data_Domain.Models;
+using Authorization.Data.Shared.DbContext;
+
+namespace Authorization.Data.Repository
+{
+    public interface IResultRepository : IRepositoryBase<Result>
+    {
+       
+
+    }
+    public class ResultRepository : RepositoryBase<Result>, IResultRepository
+    {
+        public ResultRepository(HospitalDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
