7713ea5f90a64bb05dc61a45e896eda76bbdbc9d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-13 18:17:00 +0300

    Validator result

---
+++ Microservices/Appoitment.API/Appoitment.API.Application/Validators/Command/AppointmentResult/CreateAppointmentResultValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Appointment.API.Application.Command.AppointmentResult.CreateAppointmentResult;
+using Appointment.API.Application.Resources;
+using Authorization.Data.Repository;
+using FluentValidation;
+
+namespace Appointment.API.Application.Validators.Command.AppointmentResult
+{
+    public class CreateAppointmentResultValidator : AbstractValidator<CreateAppointmentResultCommand>
+    {
+        private readonly IAppointmentRepository _appointmentRepository;
+
+        public CreateAppointmentResultValidator(IAppointmentRepository appointmentRepository)
+        {
+            _appointmentRepository = appointmentRepository;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.CreateAppointmentDTO.AppointmentId)
+                .MustAsync(_appointmentRepository.ExistsAsync)
+                .WithMessage(opt => string.Format(Messages.NotFoundAppointmentId, opt.CreateAppointmentDTO.AppointmentId));
+        }
+    }
+}
---
+++ Microservices/Appoitment.API/Appoitment.API.Application/Validators/Command/AppointmentResult/UpdateAppointmentResultValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Appointment.API.Application.Command.AppointmentResult.UpdateAppointmentResult;
+using Appointment.API.Application.Resources;
+using Authorization.Data.Repository;
+using FluentValidation;
+
+namespace Appointment.API.Application.Validators.Command.AppointmentResult
+{
+    public class UpdateAppointmentResultValidator:AbstractValidator<UpdateAppointmentResultCommand>
+    {
+        private readonly IResultRepository _resultRepository;
+
+        public UpdateAppointmentResultValidator(IResultRepository resultRepository)
+        {
+            _resultRepository= resultRepository;
+            CreateRules();
+        }
+
+        private void CreateRules()
+        {
+            RuleFor(x => x.NewResult.AppointmentResultId)
+                .MustAsync(_resultRepository.ExistsAsync)
+                .WithMessage(opt =>
+                    string.Format(Messages.NotFoundAppointmentResultId, opt.NewResult.AppointmentResultId));
+        }
+    }
+}
