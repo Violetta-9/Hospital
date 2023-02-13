b7acda9e0ee82633858d5e53a4e0d10b7185f449
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-02-13 19:19:53 +0300

    Get serviceCategories query

---
+++ Microservices/Services.API/Services.API.Application/Query/GetServiceCategories/GetServiceCategoriesQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Authorization.Data.Repository;
+using MediatR;
+using Services.API.Contracts.Outgoing;
+
+namespace Services.API.Application.Query.GetServiceCategories
+{
+    internal class GetServiceCategoriesQueryHandler : IRequestHandler<GetServiceCategoriesQuery, ServiceCategoriesDTO[]>
+    {
+        private readonly IServiceCategoryRepository _serviceCategoryRepository;
+
+        public GetServiceCategoriesQueryHandler(IServiceCategoryRepository serviceCategoryRepository)
+        {
+            _serviceCategoryRepository= serviceCategoryRepository;
+        }
+
+        public async Task<ServiceCategoriesDTO[]> Handle(GetServiceCategoriesQuery request, CancellationToken cancellationToken)
+        {
+           return await _serviceCategoryRepository.GetServiceCategories(cancellationToken);
+        }
+    }
+}
