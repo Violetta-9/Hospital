5818d27d9f727ba8fb18cf383e6a9a120ba8c71d
vitusjzachar@gmail.com <vitusjzachar@gmail.com>
2023-10-10 12:28:11 +0300

    Small changes

--- Microservices/Specialization.API/Specialization.Tests/IntegrationBaseTest.cs
+++ Microservices/Specialization.API/Specialization.Tests/IntegrationBaseTest.cs
@@ -1,15 +1,10 @@
 ﻿using Authorization.Data.Shared.DbContext;
-using Authorization.Data_Domain.Models;
-using FluentAssertions.Common;
 using FluentValidation;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Specialization.API.Application.PipelineBehaviors;
-using Specialization.API.Controllers.Abstraction.Mediator;
-using System.Reflection;
-using Specialization.API.Application;
 using Authorization.Data.Repository;
 using Services.API.Client;
 using ServiceCollectionExtension = Specialization.API.Application.ServiceCollectionExtension;
@@ -50,16 +45,6 @@ namespace Specialization.Tests
             serviceCollection.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             AssemblyScanner.FindValidatorsInAssembly(typeof(ServiceCollectionExtension).Assembly)
                 .ForEach(item => serviceCollection.AddScoped(item.InterfaceType, item.ValidatorType));
-            serviceCollection.Configure<JwtBearerOptions>(JwtBearerDefaults.AuthenticationScheme, options =>
-            {
-                var config = new OpenIdConnectConfiguration()
-                {
-                    Issuer = MockJwtTokens.Issuer
-                };
-
-                config.SigningKeys.Add(MockJwtTokens.SecurityKey);
-                options.Configuration = config;
-            });
             Services = serviceCollection.BuildServiceProvider();
             _mediator = Services.GetService<IMediator>();
 
@@ -67,25 +52,5 @@ namespace Specialization.Tests
         }
     }
 }
-public static class MockJwtTokens
-{
-    public static string Issuer { get; } = Guid.NewGuid().ToString();
-    public static SecurityKey SecurityKey { get; }
-    public static SigningCredentials SigningCredentials { get; }
 
-    private static readonly JwtSecurityTokenHandler s_tokenHandler = new JwtSecurityTokenHandler();
-    private static readonly RandomNumberGenerator s_rng = RandomNumberGenerator.Create();
-    private static readonly byte[] s_key = new byte[32];
 
-    static MockJwtTokens()
-    {
-        s_rng.GetBytes(s_key);
-        SecurityKey = new SymmetricSecurityKey(s_key) { KeyId = Guid.NewGuid().ToString() };
-        SigningCredentials = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
-    }
-
-    public static string GenerateJwtToken(IEnumerable<Claim> claims)
-    {
-        return s_tokenHandler.WriteToken(new JwtSecurityToken(Issuer, null, claims, null, DateTime.UtcNow.AddMinutes(20), SigningCredentials));
-    }
-}
